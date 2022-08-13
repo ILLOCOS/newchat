af710566ea1ce926e15b6e3dae20350636aded0a
dax <audaxvillegas3@gmail.com>
2022-08-13 14:21:01 +0800

    added new chat folder

---
+++ constants.js
@@ -0,0 +1,61 @@
+// Options the user could type in
+const prompts = [
+  ["what is computer"],
+  ["what is computer casing"],
+  ["what is monitor"],
+  ["what is keyboard"],
+  ["what is mouse"],
+  ["what is power supply"],
+  ["what is motherboard"],
+  ["what is cpu"],
+  ["what is hard drive"],
+  ["    "],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["before i answer your question, shoutout to my friends, namely the handsome jack villegas, grade five at vabes pasay, tom, mel, dax, ben, nixon, princess, jacob, jed, justine, and their teacher miss karen, at wd 13, kodego and now going back to your question, a computer is an electronic device for storing and processing data, typically in binary form, according to instructions given to it in a variable program, ok and that was so easy"],
+  ["computer casing is where all the main computer components are housed like Motherboard, processor, memory, hard disk and power supply ok is there anything else"],
+  ["monitor is where the images, text, and videos are displayed and now this is getting exciting so ask me more"],
+  ["a keyboard is where you type, enter data and a peripheral input device modeled after the typewriter keyboard which uses an arrangement of buttons or keys to act as mechanical levers or electronic switches and that was easy"],
+  ["A computer mouse is a hand-held pointing device that detects two-dimensional motion relative to a surface. This motion is typically translated into the motion of a pointer on a display, which allows a smooth control of the graphical user interface of a computer, ok is there anything else"],
+  ["A power supply unit converts mains alternating current to low-voltage regulated direct current power for the internal components of a computer and now this is getting exciting so ask me more"],
+  ["The motherboard is the backbone that ties the computer components together at one spot and allows them to talk to each other. Without it, none of the computer pieces, such as the central processing unit, Graphics processing unit or hard drive, could interact and that was pretty easy"],
+  ["The processor, also known as the CPU, provides the instructions and processing power the computer needs to do its work. The more powerful and updated your processor, the faster your computer can complete its tasks. By getting a more powerful processor, you can help your computer think and work faster such as intel i3, i5, i7 and i9 ok, is there anything else"],
+  ["The hard drive is where all your permanent computer data is stored. Whenever you save a file, photo, or software to your computer, it's stored in your hard drive. Most hard drives have storage space between 250GB and 1TB and by the way the difference between hard disk drive and solid state drive is, HDD are traditional storage devices with spinning platters that read and write data. SSD use newer technology that stores data on instantly accessible memory chips. SSD are faster, quieter, smaller, consume less energy, and more durable. So, SSD is the new cool kid in town"],
+  ["    "],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
